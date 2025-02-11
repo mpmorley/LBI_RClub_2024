129d0c03179be1371958f35fbbad0bf835893455
mpmorley <mmorley@pennmedicine.upenn.edu>
2025-02-11 10:03:08 -0500

    Create ColorPal.R

---
+++ 20250211/ColorPal.R
@@ -0,0 +1,6 @@
+
+pal.list <- list(
+  sample = c('#2b4570','#e8871e', '#8e2e29'),
+  endo = c('deepskyblue3','chocolate2','mediumorchid4'),
+  endo.lvl2 = c('deepskyblue3','lightblue','steelblue4','chocolate2','darkorange','sienna1','mediumorchid4')
+)
